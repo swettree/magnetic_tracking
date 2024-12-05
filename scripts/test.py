81a05025739b24d692d484cc4d9d7acc6b660fa4
swettree <huangding3366@163.com>
2024-12-05 15:11:00 +0800

    2412051504

--- scripts/test.py
+++ scripts/test.py
@@ -1,9 +1,16 @@
 import usb.core
 import usb.util
+import numpy as np
 
-# 查找所有连接的 USB 设备
-devices = usb.core.find(find_all=True)
 
-# 打印每个设备的信息
-for device in devices:
-    print(f"Device: ID {hex(device.idVendor)}:{hex(device.idProduct)}")
+QMC_init_position = [0.45, 0.6, 0.004]
+coil_positions = [0.1225, 0.770, 0.09]
+
+def qmc_init_r(QMC_init_position, coil_positions):
+    return np.sqrt((coil_positions[0] - QMC_init_position[0]) ** 2 + (coil_positions[1] - QMC_init_position[1]) ** 2 + (
+                coil_positions[2] - QMC_init_position[2]) ** 2)
+
+r = qmc_init_r(QMC_init_position, coil_positions)
+
+
+print(f"{r}")
